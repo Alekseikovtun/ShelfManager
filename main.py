0361056501414c7caab371570eb3993f305b2f7f
Alekseikovtun <aleksei.kovtun.earth@gmail.com>
2022-09-14 18:30:38 +0300

    add 3/4 functions

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
+from xml.dom.minidom import Document
+
+
+def get_name():
+    number = input('Введите номер документа')
+    for data in documents:
+        if data.get("number") == number:
+            return data.get('name')
+    return 'Документа с таким номером не найдено'
+
+
+def get_shelf():
+    number = input('Введите номер документа')
+    for shelf_key in directories:
+        if number in directories.get(shelf_key):
+            return shelf_key
+    return 'На полках с нет документа с таким номером'
+
+
+def get_list():
+    for docs in documents:
+        return docs
+
+
+def add_doc():
+    shelf = input('Введите номер полки, куда хотите положить документ: ')
+    if shelf not in directories:
+        return 'Нет такой полки'
+    doc = {}
+    for docs_info in ('type, )
+    return
+
 
 
 documents = [
@@ -11,3 +43,4 @@ directories = {
     '2': ['10006'],
     '3': []
 }
+
